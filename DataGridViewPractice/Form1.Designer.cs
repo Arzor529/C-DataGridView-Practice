c5e16e635ce6b957306fcfe4626ab275845035ee
Aaron Keel <aaron.keel@hotmail.co.uk>
2021-03-01 17:02:18 +0000

    First Commit

---
+++ DataGridViewPractice/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DataGridViewPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPersonId = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtSecondName = new System.Windows.Forms.TextBox();
+            this.txtProfession = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 252);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(728, 299);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PersonID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Second Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Profession";
+            // 
+            // txtPersonId
+            // 
+            this.txtPersonId.Location = new System.Drawing.Point(31, 55);
+            this.txtPersonId.Name = "txtPersonId";
+            this.txtPersonId.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonId.TabIndex = 5;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(31, 118);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // txtSecondName
+            // 
+            this.txtSecondName.Location = new System.Drawing.Point(155, 118);
+            this.txtSecondName.Name = "txtSecondName";
+            this.txtSecondName.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondName.TabIndex = 7;
+            // 
+            // txtProfession
+            // 
+            this.txtProfession.Location = new System.Drawing.Point(31, 180);
+            this.txtProfession.Name = "txtProfession";
+            this.txtProfession.Size = new System.Drawing.Size(224, 20);
+            this.txtProfession.TabIndex = 8;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(31, 206);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(112, 206);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(193, 206);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 12;
+            this.btnRefresh.Text = "Refresh ";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtProfession);
+            this.Controls.Add(this.txtSecondName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtPersonId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPersonId;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtSecondName;
+        private System.Windows.Forms.TextBox txtProfession;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ DataGridViewPractice/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridViewPractice
+{
+    public partial class Form1 : Form
+    {
+
+        public List<Person> People { get; set; }
+
+        public Form1()
+        {
+            People = GetPeople();
+            InitializeComponent();
+        }
+
+        private List<Person> GetPeople()
+        {
+            var list = new List<Person>();
+
+            list.Add(new Person()
+            {
+                PersonId = 1,
+                Name = "Bob", 
+                Surname = "Smith",
+                Profession = "Programmer"
+            });
+
+            list.Add(new Person()
+            {
+                PersonId = 2,
+                Name = "John",
+                Surname = "Wayne",
+                Profession = "Salesman"
+            });
+
+            return list;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var people = this.People;
+
+            dataGridView1.DataSource = people;
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                var selectedPerson = dataGridView1.SelectedRows[0].DataBoundItem as Person;
+                txtPersonId.Text = selectedPerson.PersonId.ToString();
+                txtFirstName.Text = selectedPerson.Name;
+                txtSecondName.Text = selectedPerson.Surname;
+                txtProfession.Text = selectedPerson.Profession;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An Error Occurred: " + ex.Message + " - " + ex.Source);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var selectedPerson = dataGridView1.SelectedRows[0].DataBoundItem as Person;
+            var selectedPersonId = selectedPerson.PersonId;
+
+            int textPersonId = Convert.ToInt32(txtPersonId.Text);
+            try
+            {
+                if (selectedPersonId == textPersonId) {
+                    this.People[selectedPersonId - 1].Name = txtFirstName.Text;
+                    this.People[selectedPersonId - 1].Surname = txtSecondName.Text;
+                    this.People[selectedPersonId - 1].Profession = txtProfession.Text;
+                    MessageBox.Show("Existing record succesfully updated! \n Please click 'Refresh' to see changes", "Success!");
+                } 
+                else
+                {
+                    MessageBox.Show("Exisitng PersonID cannot be changed!", "Error!");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An Error Occurred: " + ex.Message + " - " + ex.Source);
+            }
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            nullFields();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            var selectedPerson = dataGridView1.SelectedRows[0];
+
+            nullFields();
+
+            selectedPerson.Selected = false;
+
+            dataGridView1.DataSource = this.People;
+
+        }
+
+        private void nullFields()
+        {
+            txtPersonId.Text = null;
+            txtFirstName.Text = null;
+            txtSecondName.Text = null;
+            txtProfession.Text = null;
+        }
+
+    }
+}
